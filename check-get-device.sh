32cf760d12fb56a21f0eaaf1643d5bc4131bd7ab
Square Root of Saturn <sqrt@octoblu.com>
2015-12-15 11:29:55 -0700

    pseudo-tests for /v3/devices and /v2/whoami

--- check-get-device.sh
+++ check-get-device.sh
@@ -2,14 +2,14 @@
 MESHBLU_SERVER=meshblu.octoblu.com
 MESHBLU_HTTP_SERVER=meshblu.octoblu.com
 
-AUTH_DEVICE=`meshblu-util register -s $MESHBLU_SERVER  -t aaron:sucks `
+AUTH_DEVICE=`meshblu-util register -s $MESHBLU_SERVER  -t device:auth `
 AUTH_DEVICE_UUID=`echo $AUTH_DEVICE | jq -r '.uuid'`
 AUTH_DEVICE_TOKEN=`echo $AUTH_DEVICE | jq -r '.token'`
 
-DISCOVERER=`meshblu-util register -s $MESHBLU_SERVER -d "{ \"discoverAsWhitelist\": [\"$AUTH_DEVICE_UUID\"]}" -t great:ya`
+DISCOVERER=`meshblu-util register -s $MESHBLU_SERVER -d "{ \"discoverAsWhitelist\": [\"$AUTH_DEVICE_UUID\"]}" -t device:discoverer`
 DISCOVERER_UUID=`echo $DISCOVERER | jq -r '.uuid'`
 
-DISCOVEREE=`meshblu-util register -s $MESHBLU_SERVER -d "{ \"discoverWhitelist\": [\"$DISCOVERER_UUID\"] }" -t peter:rocks`
+DISCOVEREE=`meshblu-util register -s $MESHBLU_SERVER -d "{ \"discoverWhitelist\": [\"$DISCOVERER_UUID\"] }" -t device:discoveree`
 DISCOVEREE_UUID=`echo $DISCOVEREE | jq -r '.uuid'`
 
 echo "AUTH_DEVICE: "
@@ -21,10 +21,18 @@ echo $DISCOVERER | jq '.'
 echo "DISCOVEREE: "
 echo $DISCOVEREE | jq '.'
 
+echo "get discoverer should fail:"
+curl -v http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v3/devices/$DISCOVERER_UUID
 
-# curl -vvv http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v2/devices/$DISCOVERER_UUID
-# curl -vvv http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v2/devices/$DISCOVERER_UUID -H "X-AS: $DISCOVERER_UUID"
-# curl http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v2/devices/$DISCOVEREE_UUID -H "X-AS: $DISCOVERER_UUID" | jq '.'
-# curl -vvv http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v2/whoami -H "X-AS: $DISCOVERER_UUID"
-curl -vvv http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v2/whoami -H "X-AS: $DISCOVERER_UUID"
-# curl -vvvv http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v2/whoami -H "X-AS: $DISCOVERER"
+echo "get discoverer as discoverer device should work:"
+curl http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v3/devices/$DISCOVERER_UUID -H "X-AS: $DISCOVERER_UUID" | jq '.type'
+echo "get discoveree as discoverer device should work:"
+curl http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v3/devices/$DISCOVEREE_UUID -H "X-AS: $DISCOVERER_UUID" | jq '.type'
+echo "whoami as auth device get auth:"
+curl http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v2/whoami | jq '.type'
+
+echo "whoami as discoverer device get discoverer:"
+curl http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v2/whoami -H "X-AS: $DISCOVERER_UUID" | jq '.type'
+
+echo "whoami as discoveree device should fail:"
+curl -v http://$AUTH_DEVICE_UUID:$AUTH_DEVICE_TOKEN@$MESHBLU_HTTP_SERVER/v2/whoami -H "X-AS: $DISCOVEREE_UUID"
